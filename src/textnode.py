f9e248ba5c330f9cfefc4c8e7f083659ceceba0f
ksagit <kuba.kielaszek@gmail.com>
2025-05-31 01:31:27 +0200

    text to html node conversion

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from leafnode import LeafNode
+
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -25,3 +27,20 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(TextNode):
+    if TextNode.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=TextNode.text)
+    if TextNode.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=TextNode.text)
+    if TextNode.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=TextNode.text)
+    if TextNode.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=TextNode.text)
+    if TextNode.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=TextNode.text, props={"href": TextNode.url})
+    if TextNode.text_type == TextType.IMAGE:
+        return LeafNode(tag="img", value="", props={"src": TextNode.url, "alt": TextNode.text})
+    else:
+        raise Exception("Invalid TextType")
